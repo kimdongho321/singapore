3b076e7a4165a32150243a445972d4fbe6846420
KIMDONGHO <ehdrh321@gmail.com>
2023-02-17 21:15:16 +0900

    02/17 21:16 feat : 프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react';
+import {useHistory} from "react-router";
+import axios from "axios";
+import {BrowserRouter,Routes, Route,Link} from 'react-router-dom';
+import Main from "./component/Main"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+            <Route exact path = '/' element={<Main />}/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
