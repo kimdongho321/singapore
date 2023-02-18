f37872e9b58e7e01ea32ae7fc95eaad53d57dcca
KIMDONGHO <ehdrh321@gmail.com>
2023-02-19 01:05:12 +0900

    02/19 01:04 refactor : 메인 페이지 퍼블리싱(초안1)

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import "../style/Main.css";
+import bgimg from "../asset/image/main-bg.jpg";
 
 export default function Main(){
 
     return(
-        <div className='main'>
-            ??
-        </div>
+        <>
+            <div className='bg-img'>
+                <img src={bgimg}/>
+                <div className='bg-title'></div>
+            </div>
+            <div className='bg-gradation'>
+                <div className='bg-border'></div>
+            </div>
+        </>
     )
 
 }
