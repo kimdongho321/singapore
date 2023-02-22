8bc032b0befebd628d3dd53351aa7e50d74776ed
KIMDONGHO <ehdrh321@gmail.com>
2023-02-22 16:27:20 +0900

    02/22 16:26 reafactor : Day1.js에서 Travle.js로 setMode props를 전달하도록 코드 수정

--- src/component/Day1/Day1.js
+++ src/component/Day1/Day1.js
@@ -1,51 +1,46 @@
-import React, { useEffect ,useState} from "react";
+import React, { useEffect, useState } from "react";
 import AOS from "aos";
-import "../../style/Day1.css"
+import "../../style/Day1.css";
 import "aos/dist/aos.css";
-import airplane from "../../asset/image/Day1/airplane.png"
-import airport from "../../asset/image/Day1/airport.png"
-import Travle from "./Travle.js"
-import Album from "./Album.js"
-
+import airplane from "../../asset/image/Day1/airplane.png";
+import airport from "../../asset/image/Day1/airport.png";
+import Travle from "./Travle.js";
+import Album from "./Album.js";
 
 export default function Day1(){
-
     const [mode, setMode] = useState('wait');
-    const [ style, setStyle ] = useState({display: 'block'})
-
+    const [style, setStyle] = useState({display: 'block'})
 
     useEffect(() => { 
         AOS.init(); 
-      }, []); 
-      
-      function conditionRender(conditionMode) {
+    }, []); 
+
+    const pageload = () => {
+        setMode("travle")
+        setStyle({display: 'none'})
+    }
+
+    const conditionRender = (conditionMode) => {
         if (conditionMode === "travle") {
-            return <Travle/>
+            return <Travle setMode={setMode} />
         } else if (conditionMode === "album") {
-            return <Album/>
+            return <Album />
         }
     }
 
-    const pageload = () => {
-        setMode("travle");
-        setStyle(({display: 'none'}))
-        console.log(mode);
-      }
     return(
         <>
-            <div className="day1">
-                <div className="airport-ani" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="1000" style={style}>
+            <div className="day1" style={style}>
+                <div className="airport-ani" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="1000">
                     <div className="airplane" data-aos="zoom-in">
                         <img src={airplane}/>
                     </div>
                     <div className="airport" data-aos="fade-up-right" data-aos-delay="500" data-aos-duration="1500">
-                            <img src={airport} onClick={pageload} /> 
+                        <img src={airport} onClick={pageload} /> 
                     </div>
-                </div>
-                {conditionRender(mode)}
+                </div>  
             </div>
-            
+            {conditionRender(mode)}
         </>
-        
     )
 }
