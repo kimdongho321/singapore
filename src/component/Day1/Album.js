f5f09cc7d764f909b39ab039d6c7138f510d5231
KIMDONGHO <ehdrh321@gmail.com>
2023-02-22 16:30:59 +0900

    02/22 16:30 chore : Album.js 렌더링 테스트

---
+++ src/component/Day1/Album.js
@@ -0,0 +1,16 @@
+import React, { useEffect ,useState} from "react";
+import "../../style/Album.css";
+import airport from "../../asset/image/Day1/airport.png"
+import earth from "../../asset/image/Day1/earth.png"
+import singapore from "../../asset/image/Day1/singapore.png"
+
+export default function Album(){
+
+    return(
+        <div className="travle" >
+            <div className="travle-ani" >
+                ???
+            </div>
+        </div>
+    )
+}
