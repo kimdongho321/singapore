f08a8d9cffe5b97bdecb99c65c3918b8dd52fd25
KIMDONGHO <ehdrh321@gmail.com>
2023-02-22 16:28:56 +0900

    02/22 16:27 feat : earth img 클릭시 state를 album으로 변경해 Album.js렌더링 기능 추가

--- src/component/Day1/Travle.js
+++ src/component/Day1/Travle.js
@@ -1,20 +1,39 @@
 import React, { useEffect ,useState} from "react";
-import "../../style/Day1.css";
+import "../../style/Travle.css";
 import airport from "../../asset/image/Day1/airport.png"
 import earth from "../../asset/image/Day1/earth.png"
 import singapore from "../../asset/image/Day1/singapore.png"
+import Album from "./Album.js"
 
 export default function Travle(){
+    const [style, setStyle] = useState({display: 'block'})
+    const [mode, setMode] = useState('wait')
 
-    return(
-        <div className="travle">
-            <div className="travle-ani">
-                <div className="travle-airport">
-                    <img src={airport}/>
+    const conditionRender = (conditionMode) => {
+        if (conditionMode === "album") {
+            return <Album />
+        }
+    }
+
+    const pageload = () => {
+        setMode("album")
+        setStyle({display: 'none'})
+    }
+
+    return (
+        <>
+            <div className="travle" style={style}>
+                <div className="travle-ani">
+                    <div className="travle-airport">
+                        <img src={airport} onClick={pageload}/>
+                    </div>
+                    <div className="earth-div">
+                        <img src={earth} className="earth"/>
+                    </div>
+                    <img src={singapore} className="singapore"/>
                 </div>
-                <img src={earth}/>
-                <img src={singapore} className="singapore"/>
             </div>
-        </div>
+            {conditionRender(mode)}
+        </>
     )
 }
